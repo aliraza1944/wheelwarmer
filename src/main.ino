e372943b652293a7113176974e7f8894bd008661
aliraza1944 <aliraza1944@gmail.com>
2017-09-16 15:01:01 +0500

    removed error detection part

--- src/main.ino
+++ src/main.ino
@@ -1,3 +1,14 @@
+/*
+TODO:
+
+  1. Work on error detection.
+      -Read ACS712 Correctly
+  2. Add Relay
+  3. Add HC-05
+
+ */
+
+
 #include "Arduino.h"
 #include <LiquidCrystal_I2C.h>
 #include <DHT.h>
@@ -14,6 +25,8 @@ Pin definitions
 /*
 ADC Pin Definitions
  */
+
+#if 0
  #define CS_PIN D0    //PADS 1-8
  #define CS2_PIN D4   //PADS 9-16
  #define CLOCK_PIN 14
@@ -23,7 +36,7 @@ ADC Pin Definitions
  #define ADC_SCALE 1023.0
  #define VREF 5.0
  #define sensitivity 0.185
-
+#endif
 /*
 Flags Initialization
  */
@@ -48,8 +61,10 @@ DHT dht(DHTPIN, DHTTYPE);
 /*
 MCP3008 Initialization
  */
+#if 0
 MCP3008 adc(CLOCK_PIN, MOSI_PIN, MISO_PIN, CS_PIN);
 MCP3008 adc2(CLOCK_PIN, MOSI_PIN, MISO_PIN, CS2_PIN);
+#endif
 
 /*
 Global Variables
@@ -57,12 +72,14 @@ Global Variables
 float temperature = 0.0;
 int zeroFactor[31] = {0};   //ZeroFactor for all attached ACS712s. Set thru calibrateAll Function.
 float current[31] = {0.00}; //Current Sensed for all attached ACS712s.
+
 /*
 Function Declarations
  */
+#if 0
 void calibrateAll();   //Calibrates all 32 ACS712 Sensors.
 void getCurrent();     //Gets Current from all attached ACS712s.
-
+#endif
 
 void setup(){
   Serial.begin(115200);
@@ -106,10 +123,6 @@ void loop(){
 
   }
 
-  getCurrent();
-  Serial.println("***************");
-  Serial.println(current[0]);
-  Serial.println(current[8]);
   delay(1000);
 
 }
@@ -122,28 +135,20 @@ void tempFlag(){
   //Serial.println("Flag set");
 }
 
+
+#if 0
 void calibrateAll(){
   uint16_t acc[31] = {0};
-  for(int i = 0; i <= 10 ; i++){
-    //for ACS712 0-7
-    acc[0] += adc.readADC(0);
-    acc[1] += adc.readADC(1);
-    acc[2] += adc.readADC(2);
-    acc[3] += adc.readADC(3);
-    acc[4] += adc.readADC(4);
-    acc[5] += adc.readADC(5);
-    acc[6] += adc.readADC(6);
-    acc[7] += adc.readADC(7);
-    //for ACS712 8-15
-    acc[8] += adc2.readADC(0);
-    acc[9] += adc2.readADC(1);
-    acc[10] += adc2.readADC(2);
-    acc[11] += adc2.readADC(3);
-    acc[12] += adc2.readADC(4);
-    acc[13] += adc2.readADC(5);
-    acc[14] += adc2.readADC(6);
-    acc[15] += adc2.readADC(7);
-  }
+
+  for(int j = 0; j <= 7; j++)
+    for(int i = 0; i <= 10 ; i++){
+      acc[j] += adc.readADC(j);
+    }
+
+  for(int j = 0; j <= 7; j++)
+    for(int i = 0; i <= 10 ; i++){
+      acc[j+8] += adc2.readADC(j);
+    }
 
   for(int i = 0; i <= 31; i++){
     zeroFactor[i] = acc[i]/10;
@@ -189,3 +194,4 @@ void getCurrent(){
   current[0] = (float)acc[0] / 10.0 / ADC_SCALE * VREF / sensitivity;
   current[8] = (float)acc[8] / 10.0 / ADC_SCALE * VREF / sensitivity;
 }
+#endif
